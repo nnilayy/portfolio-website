ee7da1a9cf61dc5e7a58b8f0a0469623807038b8
unknown <nnilayy.work@gmail.com>
2024-09-18 03:30:35 +0530

    Fixed Blob Component for Mobile Display

--- frontend/src/Blob.js
+++ frontend/src/Blob.js
@@ -2,25 +2,15 @@
 import React from 'react';
 import './styles/Blob.css';
 
-const Blob = ({ color, size, top, left, delay }) => (
-  <div
-    className="blob"
-    style={{
-      backgroundColor: color,
-      width: size,
-      height: size,
-      top,
-      left,
-      animationDelay: `${delay}s`,
-    }}
-  />
+const Blob = ({ className }) => (
+  <div className={`blob ${className}`} />
 );
 
 export const BlobContainer = () => (
   <div className="blob-container">
-    <Blob color="#7e7b77" size="30vw" top="10%" left="5%" delay={0} />
-    <Blob color="rgba(105, 42, 213)" size="25vw" top="20%" left="45%" delay={3} />
-    <Blob color="rgb(216, 216, 42)" size="30vw" top="20%" left="60%" delay={5} />
+    <Blob className="blob-1" />
+    <Blob className="blob-2" />
+    <Blob className="blob-3" />
   </div>
 );
 
